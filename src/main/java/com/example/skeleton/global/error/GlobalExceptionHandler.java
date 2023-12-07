fe62c6f3b77316c6b148ba530cae045828678cbe
Yoojin Lee <yoojinLee.dev@gmail.com>
2023-12-08 07:32:21 +0900

    feat: Error 공통 처리 부분 추가
    - validation 추가

---
+++ src/main/java/com/example/skeleton/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.skeleton.global.error;
+
+import com.example.skeleton.global.error.exception.BusinessException;
+import com.example.skeleton.global.error.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
+        log.error("handleEntityNotFoundException", e);
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+}
