7aa5d3f48865f13c04939f4d30156bfdf6fa41c7
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-11-05 03:23:39 +0900

    feat: Change auth class name

--- src/main/java/com/example/skeleton/domain/authority/controller/AuthenticationApiController.java
+++ src/main/java/com/example/skeleton/domain/authority/controller/AuthenticationApiController.java
@@ -2,6 +2,7 @@ package com.example.skeleton.domain.authority.controller;
 
 import com.example.skeleton.domain.authority.dto.SignUpDto;
 import com.example.skeleton.domain.authority.service.AuthenticationService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/authentication")
 @RestController
-public class AuthorityApiController {
+public class AuthenticationApiController {
 
     private final AuthenticationService authenticationService;
 
-    public AuthorityApiController(AuthenticationService authenticationService) {
+    public AuthenticationApiController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
+    @PostMapping
+    public ResponseEntity<Object> signIn() {
+        return ResponseEntity.ok(null);
+    }
+
     @PostMapping("/sign-up")
-    public ResponseEntity<SignUpDto.Response> signUp(@RequestBody SignUpDto.Request request) {
+    public ResponseEntity<SignUpDto.Response> signUp(@RequestBody @Valid SignUpDto.Request request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.authenticationService.signUp(request));
     }
 
